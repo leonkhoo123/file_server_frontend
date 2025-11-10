30352f53cf3909f7debb207a944ebb4fe52fa7d8
LEON <kjl7966@gmail.com>
2025-11-10 13:28:56 +0800

    enhance pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,23 @@ import './index.css'
 import App from './App.tsx'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { toast } from 'sonner'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    toast.message('New version available', {
+      description: 'Click to reload the app.',
+      action: {
+        label: 'Reload',
+        onClick: () => updateSW(true),
+      },
+    })
+  },
+  onOfflineReady() {
+    toast.success('New App is ready!')
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
 <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -11,7 +11,35 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-  ,tailwindcss()],
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate', // detects new version automatically
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'robots.txt'],
+      manifest: {
+        name: 'Watcher',
+        short_name: 'Watcher',
+        start_url: '/home',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: "/images/logo-removebg-preview.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/images/logo-removebg-preview.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

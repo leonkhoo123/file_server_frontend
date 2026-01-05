790fee5b50da7f1cd50e8d1ab899e560ffb2d023
LEON <kjl7966@gmail.com>
2026-01-05 19:35:42 +0800

    change url building method

--- src/api/axiosLayer.tsx
+++ src/api/axiosLayer.tsx
@@ -2,13 +2,13 @@
 import axios from 'axios';
 
 // window.location.hostname returns 'localhost' or '192.168.1.10', etc.
-const hostname = window.location.hostname;
+// const hostname = window.location.hostname;
 
 // Define your ports based on your setup
-const PORT = "30333"; 
+// const PORT = "30333"; 
 
 // Construct the baseURL dynamically
-const baseURL = import.meta.env.VITE_BUILD_PROFILE === "local"? "http://localhost:3333/api" : `${window.location.protocol}://${hostname}:${PORT}/api`
+const baseURL = import.meta.env.VITE_BUILD_PROFILE === "local"? "http://localhost:3333/api" : `${window.location.protocol}/api`
 
 const instance = axios.create({
   baseURL: baseURL,
